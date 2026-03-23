91c48d1e7e41d19572e055a287458741e565af76
Nitinjohri <nitinjohri12@gmail.com>
2026-03-23 11:11:59 +0530

    initial commit

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -1,3 +1,17 @@
-from . import database, models, crud
+from . import database as database
+from . import models as models
+from . import crud as crud
 from . import crud as CRUD  # Compatibility alias
 from .models import SKU, Inventory, Forecast, PurchaseOrder, PipelineRun
+
+__all__ = [
+    "database",
+    "models",
+    "crud",
+    "CRUD",
+    "SKU",
+    "Inventory",
+    "Forecast",
+    "PurchaseOrder",
+    "PipelineRun",
+]
